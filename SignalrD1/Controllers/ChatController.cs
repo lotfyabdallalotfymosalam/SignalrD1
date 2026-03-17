086c1a1c490d0dc6f241392c740a7ea0ab204ddd
Lotfy Mosalam <lotfyabdallamosalam@gmail.com>
2026-03-17 15:07:15 +0200

    Add project files.

---
+++ SignalrD1/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalrD1.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalrD1/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrD1.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        // This method will be called by clients to send a message
+        // It takes the sender's name and the message as parameters
+        // It will save the message in the database (not implemented here) and then broadcast it to all connected clients
+        // The Clients property is provided by the Hub base class and allows us to send messages to clients
+        // The SendAsync method takes the name of the client method to call ("NewMessage") and the parameters to pass to that method (name and message)
+        // The client method "NewMessage" will be defined in the JavaScript code on the client side to handle incoming messages
+        // For example, in JavaScript, we might have something like:    
+        // connection.on("NewMessage", function(name, message) {
+        //    // code to display the message in the chat UI
+        // });
+        
+        public void SendMessage(string name, string message)
+        {
+            // save in database
+
+
+            // broadcast to all clients
+            Clients.All.SendAsync("NewMessage", name, message);
+        }
+    }
+}
